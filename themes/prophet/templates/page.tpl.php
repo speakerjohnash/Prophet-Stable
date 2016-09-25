72c1482a2b76a89e69ba54ab633aff5e314fb086
Matt Sevrens <msevrens@gmail.com>
2016-09-25 13:58:26 -0700

    syncing this version closer to beta

--- themes/prophet/templates/page.tpl.php
+++ themes/prophet/templates/page.tpl.php
@@ -78,7 +78,7 @@
   <div class="container">
     <div class="navbar-header">
       <?php if ($logo): ?>
-      <a class="logo color-predict navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
       <?php endif; ?>
